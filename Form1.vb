9ce9041fa8aab3841d3fc7da6052740d58bf3c73
qotpixie <35668422+qotpixie@users.noreply.github.com>
2018-01-21 20:36:41 +0000

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub RectangleShape1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RectangleShape1.Click
+    
+End Class
